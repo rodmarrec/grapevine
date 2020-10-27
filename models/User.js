ce657e274d8bc2decba6e6ee08edfc1abbf8b102
rodmarrec <romarron01@gmail.com>
2020-10-26 17:44:03 -0700

    adds User model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
-
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        jobTitle: { type: String, default: 'open to work' },
+        company: { type: String, default: 'n/a' },
+        photo: { type: Buffer, default: 'https://imgur.com/7OXMxkE' },
+        summary: { type: String },
+        userType: { type: String },
+        connections: [],
+        pendingRequest: [],
+    }
+);
 
 
 const User = mongoose.model('User', userSchema);
