20c095ab97d82c3508f9f84aa68eca4f8d56a902
rodmarrec <romarron01@gmail.com>
2020-10-29 22:58:45 -0700

    further changes to profile page

--- models/Profile.js
+++ models/Profile.js
@@ -7,6 +7,7 @@ const profileSchema = new mongoose.Schema(
             [true, "You must enter your First and Last Name" ] },
         jobTitle: { type: String, default: "open to work" },
         company: { type: String, default: "n/a" },
+        location: { type: String, default: "n/a" },
         photo: { type: Buffer, default: "https://imgur.com/7OXMxkE" },
         summary: { type: String },
         profileType: { type: String },
