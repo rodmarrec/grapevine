bcfd095e0ffd9f6038e3ce669fe229076a082e2e
rodmarrec <romarron01@gmail.com>
2020-10-30 17:58:45 -0700

    sets up server.js and server listener is working

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    profile: require("./profile"),
-    message: require("./message"),
-    auth: require("./auth"),
-};
--- server.js
+++ server.js
@@ -1,90 +1,34 @@
-// external imports
-const express = require("express"); 
+// External
+const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
 
 
-// internal imports
-const db = require("./models");
-const routes = require("./routes");
+// Internal
+const messageController = require("method-override");
 
 
-// instanced modules
+// Instanced
 const app = express();
 
 
 // configuration
-require("dotenv").config();
-// console.log(process.env);
-const PORT = process.env.PORT || 4000;
-app.set("view engine", "ejs"); 
+const PORT = 4000;
 
 
-// middleware
+// Middleware
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-app.use(
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: "grapes",
-        store: new MongoStore({
-            url:
-                process.env.MONGODB_URI || "mongodb://localhost:27017/grapevine",
-        }),
-        cookie: {
-            maxAge: 1000 * 60 * 24 * 7 * 2,
-        },
-    })
-);
-
-const authRequried = function (req, res, next) {
-    if (!req.session.currentUser) {
-        return res.redirect("/login");
-    }
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
     next();
-};
+})
+
+app.use("/message", messageController);
 
 
-//// views routes
-
-// home (landing-page) page route
-app.get("/", (req, res) => {
-    res.render("landing-page",{ user: req.session.currentUser });
-});
-
-// profile page route
-app.get("/profile", (req, res) => {
-    res.render("index.ejs",{ user: req.session.currentUser });
-});
-
-// search all profiles route
-app.get("/search", (req, res) => {
-    res.render("profile/show-all",{ user: req.session.currentUser });
-});
-
-// mailbox page route
-app.get("/mailbox", (req, res) => {
-    res.render("message/mailbox",{ user: req.session.currentUser });
-});
-
-
-
-//// auth routes
-app.use("/", routes.auth);
-
-//message auth routes
-app.use("/messages", routes.message);
-
-// profile routes - base url for profile resource
-app.use("/profile", routes.profile);
-
-
-
-// server listener
+// Server Listener
 app.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`);
-});
+    console.log(`Listening for client request on ${PORT}`);
+})
