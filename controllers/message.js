0520805070a368c5e1ecbb89252b323477776548
rodmarrec <romarron01@gmail.com>
2020-11-01 21:42:44 -0800

    fixes delete and adds update

--- controllers/message.js
+++ controllers/message.js
@@ -86,6 +86,7 @@ router.put("/:id", (req, res) => {
 // delete route
 router.delete("/:id", (req, res) => {
     db.Message.findByIdAndDelete(req.params.id, (error, deletedMessage) => {
+        console.log(deletedMessage)
         if(error) {
             console.log("error in message#delete:", error);
         } 
