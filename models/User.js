716c33bdc3c7511bccf14a3e43c980e6e66f2c27
rodmarrec <romarron01@gmail.com>
2020-11-02 10:13:03 -0800

    resets User model. removes profile model

--- models/User.js
+++ models/User.js
@@ -2,10 +2,27 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-        email: { type: String, required: [true, "You must enter an email"] },
+        username: { type: String, required: [true, "You must enter a username"], unique: true },
+        email: { type: String, required: [true, "You must enter an email"], unique: true },
         password: { type: String, required: [true, "You must enter a password" ] },
+        profilePic:  { type: String, default: "/public/assets/flaticon/user.svg"  },
+        messages: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Message"
+            },
+        ],
+        subMessages: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "SubMessage"
+            },
+        ],
     },
-    { timestamps: true }
+    { 
+        timestamps: true,
+        createdAt: "createdAt"
+    }
 );
 
 
